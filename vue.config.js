298026d554fbe44282a29525425999d9556a3e39
lior.shkiller <lior.shkiller@gmail.com>
2021-10-23 13:19:01 +0300

    Use polyfill

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,10 @@
 
 module.exports = {
+  configureWebpack: {
+    entry: {
+      polyfill: ['core-js/stable', 'regenerator-runtime/runtime']
+    }
+  },
   devServer: {
     port: 3000,
     https: true
